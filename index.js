bc4574d71166c812249ab370571967f4e8a656d1
XueSong <song.xue@seek-data.com>
2019-01-16 11:13:43 +0800

    完成功能

--- index.js
+++ index.js
@@ -1,16 +1,45 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
+const FILEPATH = 'D:/Program Files/ssr-win/gui-config.json';
+
+function changeJson(url) {
+    try {
+        // 读取
+        let config = fs.readFileSync(FILEPATH, 'utf-8');
+        config = JSON.parse(config);
+
+        // 更新
+        let index = config.serverSubscribes.findIndex(element => element.Group === "WWW.SSRSTOOL.COM");
+        if (index !== -1) {
+            config.serverSubscribes[index].URL = url;
+        } else {
+            config.serverSubscribes.push({
+                "URL": url,
+                "Group": "WWW.SSRSTOOL.COM",
+                "LastUpdateTime": new Date().getTime()
+            });
+        }
+
+        // 保存
+        let str = JSON.stringify(config, null, "\t");
+        fs.writeFileSync(FILEPATH, str);
+        console.log(`${new Date().getTime()} 更新成功`);
+    } catch (e) {
+        console.log(`${new Date().getTime()} 更新失败：${e}`);
+    }
+}
 
 (async () => {
-    const browser = await (puppeteer.launch({
-        headless: true
-    }));
+    const browser = await (puppeteer.launch());
     const page = await browser.newPage();
-    await page.goto('https://www.ssrtool.com/tool/free_ssr');
+    await page.goto('https://stool.gq/tool/free_ssr');
     //await page.waitFor(5000);
 
     const subscriptionUrl = await page.evaluate(() => {
         return $('.mdui-textfield-input').val();
     });
-    console.log(subscriptionUrl);
+
+    changeJson(subscriptionUrl);
+
     browser.close();
 })();
